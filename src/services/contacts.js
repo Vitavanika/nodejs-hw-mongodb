a039550e293c19fb1cdd376cfebe76cffc036c98
Vitavanika <vitavanika@gmail.com>
2025-06-30 10:49:21 +0200

    implement contact management with CRUD operations and error handling

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,6 @@
 import { Contact } from '../models/contact.js';
 
-export const getAllContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
-};
-
-export const getContactById = async (contactId) => {
-  const contact = await Contact.findById(contactId);
-  return contact;
-};
+export const getAllContacts = async () => Contact.find();
+export const getContactById = async (contactId) => Contact.findById(contactId);
+export const createContact = async (payload) => Contact.create(payload);
+export const deleteContact = async (contactId) => Contact.findByIdAndDelete(contactId);
